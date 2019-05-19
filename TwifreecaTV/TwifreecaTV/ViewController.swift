0d42f445c24a05ef7d54570ba647d8f45fb05f97
furabonos <readyordie3974@gmail.com>
2019-05-19 15:11:49 +0900

    first setting

---
+++ TwifreecaTV/TwifreecaTV/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TwifreecaTV
+//
+//  Created by 엄태형 on 2019. 5. 19..
+//  Copyright © 2019년 엄태형. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
