d2cf130317dcd48c4d47c77bbd6665d8b0d70ce6
LEEJAEJIN <jinlee1206@naver.com>
2019-05-23 17:56:40 +0900

    0523 Fin

---
+++ TwifreecaTV/TwifreecaTV/Source/ViewController/Main/Afreeca/Search/AfreecaSearchViewController.swift
@@ -0,0 +1,79 @@
+//
+//  AfreecaSearchViewController.swift
+//  TwifreecaTV
+//
+//  Created by Euijae Hong on 22/05/2019.
+//  Copyright © 2019 엄태형. All rights reserved.
+//
+
+import UIKit
+
+class AfreecaSearchViewController: UIViewController {
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var searchTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupInitialize()
+        
+    }
+    
+    func setupInitialize() {
+        let string = NSLocalizedString("아프리카TV 검색", comment: "Some comment")
+        searchTextField.attributedPlaceholder = NSAttributedString(string: string ,attributes: [NSAttributedString.Key.foregroundColor: UIColor.white])
+        searchTextField.delegate = self
+        searchTextField.returnKeyType = .search
+        
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.register(
+            UINib(nibName: "SearchCollectionViewCell", bundle: nil),
+            forCellWithReuseIdentifier: "SearchCollectionViewCell"
+        )
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?){
+        self.view.endEditing(true)
+    }
+    
+    
+    @IBAction func backBtn(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+}
+
+extension AfreecaSearchViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        print("ssss")
+        self.searchTextField.resignFirstResponder()
+        return true
+    }
+}
+
+extension AfreecaSearchViewController: UICollectionViewDataSource {
+    
+//    func numberOfSections(in collectionView: UICollectionView) -> Int {
+//        return 1
+//    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "SearchCollectionViewCell", for: indexPath) as! SearchCollectionViewCell
+        return cell
+    }
+}
+
+extension AfreecaSearchViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.width, height: 200)
+    }
+    
+//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+//            return CGSize(width: view.frame.width, height: 250)
+//    }
+}
---
+++ TwifreecaTV/TwifreecaTV/Source/ViewController/Main/Afreeca/Search/Cell/SearchCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  SearchCollectionViewCell.swift
+//  TwifreecaTV
+//
+//  Created by Euijae Hong on 23/05/2019.
+//  Copyright © 2019 엄태형. All rights reserved.
+//
+
+import UIKit
+
+class SearchCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
